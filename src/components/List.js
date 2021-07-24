bdc5863ea8f20790b96591e8d8ce130e577d61b5
gonzalomvillanueva <gonzalomvillanueva@users.noreply.github.com>
2021-07-24 18:40:35 -0300

    List: image, name, gender, location and random episode link

--- src/components/List.js
+++ src/components/List.js
@@ -15,7 +15,14 @@ const List = ({choice}) => {
       <ul>
         {
           choice.length > 0 ? choice.map(item => (
-            <li key={item.id}>{item.name}</li>
+              <li key={item.id}>
+                <img src={item.image} alt={item.name} /> <br />
+                Name: {item.name} <br /> 
+                Gender: {item.gender} <br /> 
+                Location: {item.location.name} <br /> 
+                Random episode: <a href={item.episode[Object.keys(item.episode)[Math.floor(Math.random()*Object.keys(item.episode).length)]]} target="blank_">{item.episode[Object.keys(item.episode)[Math.floor(Math.random()*Object.keys(item.episode).length)]]}</a>
+                
+              </li>
           )) : <p>No results</p>
         }
       </ul>
